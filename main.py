f7d7fbfac26835ac35f9491e3c5efd2c2a5443a7
mewadadeepak <63440853+mewadadeepak@users.noreply.github.com>
2024-08-11 09:21:42 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,150 @@
+# Import math Library
+import math
+'''   math.acos() method   '''
+# Return the arc cosine of numbers
+print(math.acos(0.55))
+print(math.acos(-0.55))
+print(math.acos(0))
+print(math.acos(1))
+print(math.acos(-1))
+
+print("******************************2************************************")
+# Return the inverse hyperbolic cosine of different numbers
+print (math.acosh(7))
+print (math.acosh(56))
+print (math.acosh(2.45))
+print (math.acosh(1))
+print("********************************3**********************************")
+# Return the arc sine of numbers
+print(math.asin(0.55))
+print(math.asin(-0.55))
+print(math.asin(0))
+print(math.asin(1))
+print(math.asin(-1))
+print("*********************************4*********************************")
+# Return the inverse hyperbolic sine of numbers
+print(math.asinh(7))
+print(math.asinh(56))
+print(math.asinh(2.45))
+print(math.asinh(1))
+print("*****************************5*************************************")
+#fReturn the arc tangent of different numbers
+print (math.atan(0.39))
+print (math.atan(67))
+print (math.atan(-21))
+print("*****************************6*************************************")
+# Return the arc tangent of y/x in radians
+print(math.atan2(8, 5))
+print(math.atan2(20, 10))
+print(math.atan2(34, -7))
+print("*****************************7*************************************")
+#print the hyperbolic arctangent of different numbers
+print(math.atanh(0.59))
+print(math.atanh(-0.12))
+print("*****************************8*************************************")
+# Round a number upward to its nearest integer
+print(math.ceil(1.4))
+print(math.ceil(5.3))
+print(math.ceil(-5.3))
+print(math.ceil(22.6))
+print(math.ceil(10.0))
+print("*****************************9*************************************")
+# Import math Library
+import math
+
+# Initialize the number of items to choose from
+n = 7
+
+# Initialize the number of possibilities to choose
+k = 5
+
+# Print total number of possible combinations
+print (math.comb(n, k))
+print("*************************10*****************************************")
+# Return the cosine of different numbers
+print (math.cos(0.00))
+print (math.cos(-1.23))
+print (math.cos(10))
+print (math.cos(3.14159265359))
+print("**************************11****************************************")
+
+# Convert from radians to degrees:
+print (math.degrees(8.90))
+print (math.degrees(-20))
+print (math.degrees(1))
+print (math.degrees(90))
+print("***************************12***************************************")
+#Print absolute values from numbers
+print(math.fabs(-66.43))
+print(math.fabs(-7))
+print("*****************************13*************************************")
+#Return factorial of a number
+print(math.factorial(9))
+print(math.factorial(6))
+print(math.factorial(12))
+print("****************************14**************************************")
+# Return the remainder of x/y
+print(math.fmod(20, 4))
+print(math.fmod(20, 3))
+print(math.fmod(15, 6))
+print(math.fmod(-10, 3))
+#print(math.fmod(0, 0))
+print("*****************************15*************************************")
+#find the  the greatest common divisor of the two integers
+print (math.gcd(3, 6))
+print (math.gcd(6, 12))
+print (math.gcd(12, 36))
+print (math.gcd(-12, -36))
+print (math.gcd(5, 12))
+print (math.gcd(10, 0))
+print (math.gcd(0, 34))
+print (math.gcd(0, 0))
+
+print("**************************16****************************************")
+
+#compare the closeness of two values
+print(math.isclose(1.233, 1.4566))
+print(math.isclose(1.233, 1.233))
+print(math.isclose(1.233, 1.24))
+print(math.isclose(1.233, 1.233000001))
+
+print("****************************17**************************************")
+# Check whether some values are infinite
+print (math.isinf (56))
+print (math.isinf (-45.34))
+print (math.isinf (+45.34))
+print (math.isinf (math.inf))
+print (math.isinf (float("nan")))
+print (math.isinf (float("inf")))
+print (math.isinf (float("-inf")))
+print (math.isinf (-math.inf))
+print("*************************18*****************************************")
+# Print the square root of different numbers
+print (math.sqrt(10))
+print (math.sqrt (12))
+print (math.sqrt (68))
+print (math.sqrt (100))
+print("****************************19**************************************")
+# Round square root downward to the nearest integer
+print (math.isqrt(10))
+print (math.isqrt (12))
+print (math.isqrt (68))
+print (math.isqrt (100))
+print("**********************20********************************************")
+# Return the natural logarithm of different numbers
+print(math.log(2.7183))
+print(math.log(2))
+print(math.log(1))
+
+print("*****************21*************************************************")
+# Return the base-10 logarithm of different numbers
+print(math.log10(2.7183))
+print(math.log10(2))
+print(math.log10(1))
+
+print("********************22**********************************************")
+# Return the base-10 logarithm of different numbers
+print(math.log10(2.7183))
+print(math.log10(2))
+print(math.log10(1))
+
